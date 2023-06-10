3e50a5e7e76d09275e4c521f40754fa13e7f03f5
juandeaguero <juandeaguero@gmail.com>
2023-06-11 00:33:14 +0200

    http redirect to https

--- frontend/server.js
+++ frontend/server.js
@@ -1,15 +1,26 @@
 const express = require("express");
+const http = require("http");
 const https = require("https");
 const fs = require("fs");
 
 const app = express();
+const redirectApp = express();
+
 app.use(express.static("build"));
 
+redirectApp.get("*", (req, res) => {
+  res.redirect("https://restify.ca");
+});
+
 const options = {
-    key: fs.readFileSync("privkey.pem"),
-    cert: fs.readFileSync("fullchain.pem")
+  key: fs.readFileSync("privkey.pem"),
+  cert: fs.readFileSync("fullchain.pem")
 };
 
 https.createServer(options, app).listen(443, () => {
-    console.log("HTTPS server running on port 443");
+  console.log("HTTPS server running on port 443");
+});
+
+http.createServer(redirectApp).listen(80, () => {
+  console.log("HTTP server running on port 80");
 });
